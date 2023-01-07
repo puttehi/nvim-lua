ceb2bf3135058548842df1b58a2f23832c9c3e5e
puttehi <peteeez95@gmail.com>
2023-01-07 09:26:47 +0200

    formatting

--- lua/puttehi/packer.lua
+++ lua/puttehi/packer.lua
@@ -5,77 +5,77 @@ vim.cmd.packadd('packer.nvim')
 local debug_mode = false
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-    'puttehi/poimandres',
-    branch = "puttehi-dark",
-    config = function()
-      require('puttehi_dark').setup {
-        disable_background = false
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use {
+        'puttehi/poimandres',
+        branch = "puttehi-dark",
+        config = function()
+            require('puttehi_dark').setup {
+                disable_background = false
+            }
+        end
+    }
+
+    -- treesitter shiz
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+    }
+    -- use('nvim-treesitter/playground') -- debug mode basically
+    use {
+        "nvim-treesitter/nvim-treesitter-context"
+    }
+    use { 'p00f/nvim-ts-rainbow' }
+
+    --use { 'mhartington/formatter.nvim' }
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
+    }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    use { 'norcalli/nvim-colorizer.lua' }
+
+    if debug_mode == true then
+        use { 'bfredl/nvim-luadev' }
     end
-  }
 
-  -- treesitter shiz
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  }
-  -- use('nvim-treesitter/playground') -- debug mode basically
-  use {
-     "nvim-treesitter/nvim-treesitter-context"
-  }
-  use { 'p00f/nvim-ts-rainbow' }
+    use {
+        "RishabhRD/nvim-cheat.sh",
+        requires = { 'RishabhRD/popfix' }
+    }
 
-  --use { 'mhartington/formatter.nvim' }
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
-
-  use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-
-  use { 'norcalli/nvim-colorizer.lua' }
-
-  if debug_mode == true then
-      use { 'bfredl/nvim-luadev' }
-  end
-
-  use {
-      "RishabhRD/nvim-cheat.sh",
-      requires = {'RishabhRD/popfix'}
-  }
-
-  -- noob mode
-  use { "folke/which-key.nvim" }
+    -- noob mode
+    use { "folke/which-key.nvim" }
 end)
