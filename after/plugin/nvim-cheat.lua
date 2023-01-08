233ff9af53eb5f7c4508919172a3d1f4980dfb99
puttehi <peteeez95@gmail.com>
2023-01-08 18:29:20 +0200

    cheat.sh bindings

--- after/plugin/nvim-cheat.lua
+++ after/plugin/nvim-cheat.lua
@@ -1,2 +1,7 @@
 -- Default mode to open results of :Cheat with
-vim.g.cheat_default_window_layout = 'vertical_split' -- default nil (floating window)
+vim.g.cheat_default_window_layout = 'vertical_split' -- Open output in vertical split. Default is nil -> floating window
+-- Remaps --
+vim.keymap.set("n", "<leader>Cq", ":Cheat<CR>", { desc = "Open cheat.sh query window" })
+vim.keymap.set("n", "<leader>CQ", ":Cheat<CR>", { desc = "Open cheat.sh query window" })
+vim.keymap.set("n", "<leader>Cl", ":CheatList<CR>", { desc = "Open cheat.sh list of categories" })
+vim.keymap.set("n", "<leader>CL", ":CheatList<CR>", { desc = "Open cheat.sh list of categories" })
