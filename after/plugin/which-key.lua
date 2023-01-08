6626340dc0cc7387846535dc59cdde2d69349d66
puttehi <peteeez95@gmail.com>
2023-01-08 21:23:37 +0200

    which-key styling

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -38,7 +38,7 @@ require('which-key').setup {
         scroll_up = '<c-u>', -- binding to scroll up inside the popup
     },
     window = {
-        border = "none", -- none, single, double, shadow
+        border = "single", -- none, single, double, shadow
         position = "bottom", -- bottom, top
         margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
@@ -46,8 +46,8 @@ require('which-key').setup {
     },
     layout = {
         height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
+        width = { min = 24, max = 60 }, -- min and max width of the columns
+        spacing = 4, -- spacing between columns
         align = "left", -- align columns left, center or right
     },
     ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
