09f01451fea84b96ea715e8b3a298c9a6de4cdc8
puttehi <peteeez95@gmail.com>
2023-01-08 18:02:38 +0200

    LSP remaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,17 +3,18 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'sumneko_lua',
-  'rust_analyzer',
-  'marksman',
-  'ansiblels',
-  'yamlls',
-  'terraformls',
-  'gopls'
+    'tsserver',
+    'eslint',
+    'sumneko_lua',
+    'rust_analyzer',
+    'marksman',
+    'ansiblels',
+    'yamlls',
+    'terraformls',
+    'gopls'
 })
 
+-- Lua settings --
 -- Fix Undefined global 'vim'
 lsp.configure('sumneko_lua', {
     settings = {
@@ -26,26 +27,27 @@ lsp.configure('sumneko_lua', {
 })
 
 
+-- (Auto)completion/suggestions settings --
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.complete(), -- opens window
+    ['<C-e>'] = cmp.mapping.abort(), -- closes window
+    ['<Right>'] = cmp.mapping.abort(), -- closes window
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- choose selection
+    --    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    --    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-K>'] = cmp.mapping.scroll_docs(-4), -- scroll docs up
+    ['<C-J>'] = cmp.mapping.scroll_docs(4), -- scroll docs down
 })
-
--- disable completion with tab
--- this helps with copilot setup
---cmp_mappings['<Tab>'] = nil
---cmp_mappings['<S-Tab>'] = nil
-
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
+    suggest_lsp_servers = true,
     sign_icons = {
         error = 'E',
         warn = 'W',
@@ -54,24 +56,28 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+-- Attach which-key descriptions to keymaps
+local function opts(bufnr, description)
+    local options = { buffer = bufnr, remap = false }
+    if description ~= nil then
+        for k, v in pairs({ desc = description }) do options[k] = v end
+    end
+    return options
+end
 
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
+-- LSP remaps --
+lsp.on_attach(function(_, bufnr)
 
-  --vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  --vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  --vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  --vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  --vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  --vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  --vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  --vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  --vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  --vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    -- "Global" remaps --
+    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts(bufnr, "Rename symbol under cursor")) -- rename all references under cursor
+    vim.keymap.set("i", "<C-Space>", vim.lsp.buf.signature_help, opts(bufnr, "Show signature help popup")) -- show signature help (called function signature popup)
+    vim.keymap.set("n", "<C-Space>", vim.lsp.buf.hover, opts(bufnr, "Show symbol help popup")) -- show hovering information window
+
+    -- Prefixed remaps --
+    local prefix = "<leader>l"
+    vim.keymap.set("n", prefix .. "d", vim.lsp.buf.definition, opts(bufnr, "Show symbol definitions")) -- show definitions
+    vim.keymap.set("n", prefix .. "i", vim.lsp.buf.implementation, opts(bufnr, "Show symbol implementations")) -- show implementations
+    vim.keymap.set("n", prefix .. "r", vim.lsp.buf.references, opts(bufnr, "Show symbol references")) -- show references
 end)
 
 lsp.setup()
@@ -79,4 +85,3 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true,
 })
-
