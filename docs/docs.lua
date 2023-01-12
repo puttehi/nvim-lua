9a8f297ca1ce4455cf407e021c7338a6080f96f6
puttehi <peteeez95@gmail.com>
2023-01-12 23:42:21 +0200

    Sort by mode, then by mapping

--- docs/docs.lua
+++ docs/docs.lua
@@ -31,10 +31,14 @@ local function lines_to_maps_table(lines)
     return maps_table
 end
 
-local function sort_maps_table_by_mode(maps_table)
-    table.sort(maps_table, function(lhs, rhs)
-        return string.byte(lhs.mode) < string.byte(rhs.mode)
-    end)
+local function cmp_mode_and_mapping(lhs, rhs)
+    -- concat mode and mapping to effectively sort by mode first (alphabetically)
+    return lhs.mode .. lhs.mapping < rhs.mode .. rhs.mapping
+end
+
+-- Sort by mode first, then by mapping
+local function sort_maps_table(maps_table)
+    table.sort(maps_table, cmp_mode_and_mapping)
     return maps_table
 end
 
@@ -78,11 +82,11 @@ local function write_to_file(str)
 end
 
 local maps = read_map()
-local lines_table = str_to_table_lines(maps)
-local maps_table = lines_to_maps_table(lines_table)
+local lines_as_table = str_to_table_lines(maps)
+local maps_as_table = lines_to_maps_table(lines_as_table)
 
-local sorted_by_mode = sort_maps_table_by_mode(maps_table)
+local sorted = sort_maps_table(maps_as_table)
 
-local md_table = maps_table_to_markdown_table(sorted_by_mode)
+local md_table = maps_table_to_markdown_table(sorted)
 
 write_to_file(md_table)
