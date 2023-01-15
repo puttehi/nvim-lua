b4413b828060c3a4a2424ddea678595cb4fba9cb
puttehi <peteeez95@gmail.com>
2023-01-15 06:34:19 +0200

    Add packer docs generator

--- keymap_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=docs/docs.lua
-# LSP doesn't want to load for some reason but "just adding sleep" works around it.
-# Without it loading, on_attach keybindings are lost.
-nvim --headless $SCRIPT_PATH "+sleep 1" +so +q || exit 1
-
-echo "Injected markdown table snippet to $PWD/README.md"
