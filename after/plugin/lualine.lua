c2f99458c5d64b39cfe67c788dc03a3e7f3aa701
puttehi <peteeez95@gmail.com>
2023-01-17 23:50:16 +0200

    Show winnr, leader<winnr> navigation

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,91 +1,115 @@
+-- trunc: https://gist.github.com/benfrain/97f2b91087121b2d4ba0dcc4202d252f
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+    return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then
+            return ""
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+            return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+        end
+        return str
+    end
+end
+
+local function get_winnr()
+    return vim.api.nvim_win_get_number(0)
+end
+
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = auto, -- supplied by theme
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false, -- one line per nvim or one line per nvim window
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {
-        'branch',
-        'diff',
-        {'diagnostics',
-            sources = {
-                'nvim_lsp',
-                'coc'
-            }
-        }
-    },
-    lualine_c = {
-        {
-          'filename',
-          file_status = true,      -- Displays file status (readonly status, modified status)
-          newfile_status = true,   -- Display new file status (new file means no write after created)
-          path = 3,                -- 0: Just the filename
-                               -- 1: Relative path
-                               -- 2: Absolute path
-                               -- 3: Absolute path, with tilde as the home directory
-
-          shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-                               -- for other components. (terrible name, any suggestions?)
-          symbols = {
-            modified = '[+]',      -- Text to show when the file is modified.
-            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-            unnamed = '[anon]', -- Text to show for unnamed buffers.
-            newfile = '[New]',     -- Text to show for new created file before first writting
-          }
+    options = {
+        icons_enabled = true,
+        theme = auto, -- supplied by theme
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
         },
-        'searchcount'},
-    lualine_x = {},
-    lualine_y = {'filetype'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {
-        {
-          'filename',
-          file_status = true,      -- Displays file status (readonly status, modified status)
-          newfile_status = true,   -- Display new file status (new file means no write after created)
-          path = 3,                -- 0: Just the filename
-                               -- 1: Relative path
-                               -- 2: Absolute path
-                               -- 3: Absolute path, with tilde as the home directory
-
-          shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-                               -- for other components. (terrible name, any suggestions?)
-          symbols = {
-            modified = '[+]',      -- Text to show when the file is modified.
-            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-            unnamed = '[anon]', -- Text to show for unnamed buffers.
-            newfile = '[New]',     -- Text to show for new created file before first writting
-          }
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false, -- one line per nvim or one line per nvim window
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
         }
     },
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {
-    lualine_a = {'windows'},
-    lualine_y = {'buffers'},
-    lualine_z = {'tabs'}
-  },
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+    sections = {
+        lualine_a = {
+            { "mode", fmt = trunc(80, 1, nil, true) },
+        }, lualine_b = {
+            'branch',
+            'diff'
+        },
+        lualine_c = {
+            {
+                'diagnostics',
+                sources = {
+                    'nvim_lsp'
+                }
+            },
+            {
+                'filename',
+                file_status = true, -- Displays file status (readonly status, modified status)
+                newfile_status = true, -- Display new file status (new file means no write after created)
+                path = 3, -- 0: Just the filename
+                -- 1: Relative path
+                -- 2: Absolute path
+                -- 3: Absolute path, with tilde as the home directory
+
+                shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+                -- for other components. (terrible name, any suggestions?)
+                symbols = {
+                    modified = '[unsaved]', -- Text to show when the file is modified.
+                    readonly = '[ro]', -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '[___]', -- Text to show for unnamed buffers.
+                    newfile = '[New]', -- Text to show for new created file before first writting
+                }
+            },
+            'searchcount'
+        },
+        lualine_x = {},
+        lualine_y = { 'filetype' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = { get_winnr },
+        lualine_b = {},
+        lualine_c = {
+            {
+                'filename',
+                file_status = true, -- Displays file status (readonly status, modified status)
+                newfile_status = true, -- Display new file status (new file means no write after created)
+                path = 3, -- 0: Just the filename
+                -- 1: Relative path
+                -- 2: Absolute path
+                -- 3: Absolute path, with tilde as the home directory
+
+                shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+                -- for other components. (terrible name, any suggestions?)
+                symbols = {
+                    modified = '[+]', -- Text to show when the file is modified.
+                    readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '[anon]', -- Text to show for unnamed buffers.
+                    newfile = '[New]', -- Text to show for new created file before first writting
+                }
+            }
+        },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {
+        lualine_a = { 'windows' },
+        lualine_y = { 'buffers' },
+        lualine_z = { 'tabs' }
+    },
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
--- lua/puttehi/remap.lua
+++ lua/puttehi/remap.lua
@@ -1,14 +1,21 @@
 vim.g.mapleader = " "
 
--- Jumps
+-- Jumps / navigation
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Down 1/2 page, re-center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Up 1/2 page, re-center" })
+
 vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next result, centered, unfolded" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to prev result, centered, unfolded" })
+
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Jump to next error in quickfix list" })
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Jump to prev error in quickfix list" })
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Jump to next error in location list" })
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Jump to prev error in location list" })
+
+-- Move between windows 1-6
+for i = 1, 6 do
+    vim.keymap.set("n", "<leader>" .. i, i .. "<C-W>w", { desc = "Set Window " .. i .. " active" })
+end
 
 -- Copy-paste
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste but retain yank buffer" })
